351f4687196194f9bcd17db41fbb39252b0d62b2
Gustavo Batista <gustavobatistafig@hotmail.com>
2021-07-19 10:05:02 -0300

    Criando projeto

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu primeiro componente</p>
+    `
+})
+export class MeuPrimeiroComponent { }
